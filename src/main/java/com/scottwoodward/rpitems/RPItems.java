e7b3758725176ff91000d32047987cbb728573e1
Scott Woodward <scott.m.woodward@gmail.com>
2013-08-31 01:50:28 -0400

    Updated documentation of RPItems.java

--- src/main/java/com/scottwoodward/rpitems/RPItems.java
+++ src/main/java/com/scottwoodward/rpitems/RPItems.java
@@ -2,9 +2,22 @@ package com.scottwoodward.rpitems;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+/**
+ * RPItems.java
+ * Purpose: Functions as the entry point for the RPItems plugin. Takes care of initializing values, registering commands and listeners, 
+ *          and loading configuration files. 
+ * 
+ * @author Scott Woodward
+ */
 public class RPItems extends JavaPlugin {
-	
-	public void onEnable(){
-		
-	}
+
+    /**
+     * Called when the plugin is loaded by the server. Loads configuration files and initializes data structures. 
+     * 
+     * @see org.bukkit.plugin.java.JavaPlugin#onEnable()
+     */
+    @Override
+    public void onEnable(){
+
+    }
 }
